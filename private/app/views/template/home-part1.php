d467a5f79867f823be5e2de86be1aac74fac36e2
a00211748 <mjanani428@gmail.com>
2020-07-04 21:58:14 +0000

    commit

---
+++ private/app/views/template/home-part1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>My Blog</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/public/css/main.css">
+	</head>
+	<body class="is-preload homepage">
+		<div id="page-wrapper">
