390763e1897715dea699776e0fdc8b4fe515dbd0
a00211748 <mjanani428@gmail.com>
2020-07-04 22:07:08 +0000

    a

---
+++ private/app/views/template/home-part3.php
@@ -0,0 +1,26 @@
+<nav id="nav">
+								<ul>
+									<li class="current"><a href="index.html">Welcome</a></li>
+									<li>
+										
+										<ul>
+											
+											<li>
+												<a href="#">Phasellus consequat</a>
+												<ul>
+													<li><a href="#">Lorem ipsum dolor</a></li>
+													<li><a href="#">Phasellus consequat</a></li>
+													<li><a href="#">Magna phasellus</a></li>
+													<li><a href="#">Etiam dolore nisl</a></li>
+												</ul>
+											</li>
+											<li><a href="#">Veroeros feugiat</a></li>
+										</ul>
+									</li>
+									<li><a href="left-sidebar.html">Left Sidebar</a></li>
+									<li><a href="right-sidebar.html">Right Sidebar</a></li>
+									</ul>
+							</nav>
+
+					</header>
+				</div>
