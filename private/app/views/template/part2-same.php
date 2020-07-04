f0286c7827d9eb8ae246525743ed5b67c0d76cc7
a00211748 <mjanani428@gmail.com>
2020-07-04 22:00:09 +0000

    com

---
+++ private/app/views/template/part2-same.php
@@ -0,0 +1,8 @@
+				<div id="header-wrapper">
+					<header id="header" class="container">
+
+						<!-- Logo -->
+							<div id="logo">
+								<h1><a href="index.html">My Blog</a></h1>
+								<span>by Janani Manoharan</span>
+							</div>
