7dc18335e9cb48c225e7b342f00a63b9b69ebf40
a00211748 <mjanani428@gmail.com>
2020-07-04 22:10:44 +0000

    c

--- private/app/views/template/footer.php
+++ private/app/views/template/footer.php
@@ -1,3 +1,21 @@
-        <script src="/public/js/app.js"></script>
-    </body>
+			<!-- Footer -->
+				
+						<div class="row">
+							<div class="col-12">
+								<div id="copyright">
+									<ul class="menu">
+										<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</footer>
+				</div>
+
+			</div>
+		<!-- Scripts -->
+
+			
+
+	</body>
 </html>
---
+++ private/app/views/template/home-part4.php
@@ -0,0 +1,45 @@
+			<!-- Banner -->
+				<div id="banner-wrapper">
+					<div id="banner" class="box container">
+						<div class="row">
+							<div class="col-7 col-12-medium">
+								<h2>Hi. This is my Story.</h2>
+								<p>Hope everyone is doing well...</p>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+
+			
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row gtr-200">
+							<div class="col-4 col-12-medium">
+
+								<!-- Sidebar -->
+									<div id="sidebar">
+										
+									</div>
+
+							</div>
+							<div class="col-8 col-12-medium imp-medium">
+
+								<!-- Content -->
+									<div id="content">
+										<section class="last">
+											<h2>So what's this all about?</h2>
+											<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus.
+												Praesent semper mod quis eget mi. Etiam eu ante risus. Aliquam erat volutpat.
+												Aliquam luctus et mattis lectus sit amet pulvinar. Nam turpis nisi
+												consequat etiam lorem ipsum dolor sit amet nullam.</p>
+											<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus. Praesent semper bibendum ipsum, et tristique augue fringilla eu. Vivamus id risus vel dolor auctor euismod quis eget mi. Etiam eu ante risus. Aliquam erat volutpat. Aliquam luctus mattis lectus sit amet phasellus quam turpis.</p>
+											
+										</section>
+									</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
