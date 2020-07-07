21ca2640b40a6822ef01ab5b7a85b242a0708909
a00211748 <mjanani428@gmail.com>
2020-07-07 05:20:08 +0000

    ok

--- private/app/views/template/left-part1.php
+++ private/app/views/template/left-part1.php
@@ -4,7 +4,7 @@
 		<title>My Blog</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="/public/css/main.css" />
 	</head>
 	<body class="is-preload left-sidebar">
 		<div id="page-wrapper">
