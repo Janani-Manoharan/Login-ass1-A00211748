4e337ddb494d1e23e79e49a3fa9f0eca6f2f2746
a00211748 <mjanani428@gmail.com>
2020-07-30 21:09:04 +0000

    ok

--- private/app/controllers/main.php
+++ private/app/controllers/main.php
@@ -19,8 +19,15 @@ class Main extends Controller {
     }
     function tabOne () {
 
+        $this->model('blogmodel');
+        $list = $this->blogmodel->getBlogLists();
+
         $this->view("template/left-part1");
         $this->view("template/part2-same");
+        foreach($list as $item){
+                            $this->view("list/index",$item);
+
+            }
         $this->view("template/left-part3");
         //$this->view("template/home-part4");
         $this->view("template/footer");
---
+++ private/app/model/blogmodel.php
@@ -0,0 +1,35 @@
+<?php
+class BlogModel extends Model{
+    function __construct()
+    {
+        parent::__construct();
+        $this->db = new PDO("mysql:host=db;dbname=data", "root","root");
+    }
+
+    function DbVersion(){
+        $sql = 'SELECT VERSION()';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $res = $stmt->fetch();
+        return $res[0];
+
+    }
+
+    function getBlogLists(){
+        $sql = 'SELECT * FROM blog_lists';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $res = $stmt->fetchAll();
+        return $res;
+    }
+
+    function getOneBlogPost($id = 1){
+        $sql = 'SELECT * FROM blog_lists where author_id = ?';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(array($id));
+        $res = $stmt->fetch();
+        return $res;
+        
+    }
+}
+?>
